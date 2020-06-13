19b4274ec64f6cf1dc440b13677c4bf2bef5453c
Tjark <tjark.petersen.foehr@googlemail.com>
2020-06-13 16:33:17 +0200

    added some grapghs and time shifting

--- pipeline.py
+++ pipeline.py
@@ -5,6 +5,7 @@ from n26_api import N26Caller
 from processing import TransactionProcessor
 import dotenv
 import pandas as pd
+import datetime
 import matplotlib.pyplot as plt
 from matplotlib import use
 use('Agg')
@@ -61,30 +62,56 @@ class Pipeline:
     def get_atm(df):
         return df[df.category == 'micro-v2-atm']
 
-    def estimate_savings_rate(self, df):
-        # TODO: define new or delete
-        s_df = self.get_savings(df)
-        print(s_df.head())
-        # first = datetime.date.today().replace(day=1)
-        # last_month = first - datetime.timedelta(days=1)
-        savings_df = s_df.amount.resample('M', how='sum')
-        # savings_df = s_df[(datetime.date(s_df['created_ts_dt']) >= datetime.date(2019, 10, 1)) &
-        #                  (datetime.date(s_df['created_ts_dt']) <= last_month)]
-        savings_monthly = savings_df
-        return savings_monthly
-
-    def get_time_series(self, df, aggregation_level, graph, start, fname):
+    def get_time_series(self, df, aggregation_level, graph, start, add_name=None):
         data = self.get_expenditures(df).sort_index(axis=0)
         start_date = pd.to_datetime(start)
         data = data[data.index >= start_date]
         result = data.resample(aggregation_level).amount.sum() * -1
+
+        fname = 'expenditures_' + aggregation_level + '_' + graph + '_' + str(start)
+        if add_name:
+            fname = fname + '-' + add_name + '.png'
+        else:
+            fname = fname + '.png'
+
+        plt.figure()
         ax = result.plot(kind=graph, figsize=(20, 10))
         ax.set_xlabel("time")
         ax.set_ylabel("expenditures")
         if graph == 'bar':
             ax.set_xticklabels([str(j.day) + '-' + str(j.month) + '-' + str(j.year) for j in result.index])
         bytes_image = io.BytesIO()
-        plt.savefig(bytes_image, format='png')
+        plt.savefig(bytes_image, format='png', bbox_inches='tight')
+        bytes_image.seek(0)
+        return [fname, bytes_image]
+
+    @staticmethod
+    def filter_by_string(df, search_string):
+        df_filtered = df[df.merchant_name.str.contains(search_string)]
+        if df_filtered.shape[0] == 0:
+            df_filtered = df[df.partner_name.str.contains(search_string)]
+        return df_filtered
+
+    @staticmethod
+    def filter_by_category(df, category_string):
+        df_filtered = df[df.category.str.contains(category_string)]
+        return df_filtered
+
+    def plot_category_mix(self, df, start, end):
+        data = self.get_expenditures(df).sort_index(axis=0)
+        start_date = pd.to_datetime(start)
+        end_date = pd.to_datetime(end)
+        data = data[(data.index >= start_date) & (data.index < end_date)]
+        result = data.groupby('category').amount.sum()/data.amount.sum()
+        result = result.to_frame().reset_index().set_index('category')
+
+        fname = 'exp_category_mix_' + str(start) + '_' + str(end) + '.png'
+        plt.figure()
+        ax = result.plot(kind='bar', figsize=(20, 10))
+        ax.set_xlabel("category")
+        ax.set_ylabel("expenditures")
+        bytes_image = io.BytesIO()
+        plt.savefig(bytes_image, format='png', bbox_inches='tight')
         bytes_image.seek(0)
         return [fname, bytes_image]
 
@@ -102,20 +129,45 @@ class Pipeline:
     def run(self):
 
         print('Number of transactions currently persisted: ', self.storage.get_document_count())
-        self.update_data()
+        # self.update_data()
         print('Number of transactions currently persisted: ', self.storage.get_document_count())
         df = self.processor.get_transactions_df()
         df = self.add_date_columns(df=df)
         df = self.strip_spaces(df=df)
 
         # define outcomes and add to the list
-        plot_0 = self.get_time_series(df=df, aggregation_level='1M', graph='bar', start='01-01-2020',
-                                      fname='monthly_exp_2020_bar.png')
-        plot_1 = self.get_time_series(df=df, aggregation_level='1M', graph='line', start='01-01-2020',
-                                      fname='monthly_exp_2020_line.png')
-        plot_2 = self.get_time_series(df=df, aggregation_level='1W', graph='line', start='01-01-2020',
-                                      fname='weekly_exp_2020_line.png')
-        output_files = [plot_0, plot_1, plot_2]
+
+        today = datetime.date.today()
+        start_this_month = today.replace(day=1)
+        start_last_month = start_this_month.replace(month=today.month - 1)
+
+        plot_0 = self.get_time_series(df=df, aggregation_level='1M', graph='bar', start='01-01-2020')
+        plot_1 = self.get_time_series(df=df, aggregation_level='1M', graph='line', start='01-01-2020')
+        plot_2 = self.get_time_series(df=df, aggregation_level='1W', graph='line', start='01-01-2020')
+        plot_3 = self.get_time_series(df=df, aggregation_level='1D', graph='line', start='01-01-2020')
+        plot_4 = self.get_time_series(df=df, aggregation_level='1M', graph='line', start='01-01-2016')
+        plot_5 = self.get_time_series(df=df, aggregation_level='1D', graph='line', start=start_last_month)
+
+        plot_6 = self.plot_category_mix(df=df, start=start_last_month, end=start_this_month)
+        plot_7 = self.plot_category_mix(df=df, start='01-01-2020', end=today)
+
+        df_supermarket = self.filter_by_category(df=df, category_string='micro-v2-food-groceries')
+        plot_8 = self.get_time_series(df=df_supermarket, aggregation_level='1M', graph='bar', start='10-01-2019', add_name='SUPERMARKET')
+        plot_9 = self.get_time_series(df=df_supermarket, aggregation_level='1W', graph='line', start='10-01-2019', add_name='SUPERMARKET')
+        plot_10 = self.get_time_series(df=df_supermarket, aggregation_level='1D', graph='line', start='10-01-2019', add_name='SUPERMARKET')
+
+        df_travel_transport = self.filter_by_category(df=df, category_string='micro-v2-travel-holidays')
+        plot_8 = self.get_time_series(df=df_travel_transport, aggregation_level='1D', graph='line', start='10-01-2019',
+                                      add_name='TRAVEL_TRANSPORT')
+
+        df_rewe = self.filter_by_string(df=df, search_string='REWE')
+        plot_11 = self.get_time_series(df=df_rewe, aggregation_level='1W', graph='line', start='10-01-2019', add_name='REWE')
+
+        df_electricity = self.filter_by_string(df=df, search_string='vivi-power GmbH')
+        plot_12 = self.get_time_series(df=df_electricity, aggregation_level='1M', graph='bar', start='10-01-2019', add_name='ELECTRICITY')
+
+        output_files = [plot_0, plot_1, plot_2, plot_3, plot_4, plot_5, plot_6, plot_7, plot_8, plot_9, plot_10,
+                        plot_11, plot_12]
         zip_file = self.get_zip_file(files=output_files)
         return zip_file
 
